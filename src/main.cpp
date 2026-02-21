ce637f0455a92a567bfbb798b941ecfb80b361c1
Gabriel <gbcostasant@gmail.com>
2026-02-21 19:06:29 -0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+int main() { return 0; }
