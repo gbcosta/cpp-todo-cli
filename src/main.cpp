0135127cf3491d96662d0b1b8bb53f13bf0acaa9
Gabriel <gbcostasant@gmail.com>
2026-05-23 19:02:01 -0300

    readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+#include <ftxui/component/app.hpp>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 
-int main() { return 0; }
+using namespace ftxui;
+
+int main() {
+  using namespace ftxui;
+
+  std::vector<std::string> entries = {
+      "Entry 1",
+      "Entry 2",
+      "Entry 3",
+  };
+  int selected = 0;
+
+  // Create a menu component
+  auto menu = Menu(&entries, &selected);
+
+  // You can decorate components using the pipe operator.
+  auto component = menu | border;
+
+  // Start the main loop
+  auto screen = App::Fullscreen();
+  screen.Loop(component);
+
+  return 0;
+}
